35bae0fd50d506f6a4679e538071a23615dcce96
Santiago San Roman <santi72alc@gmail.com>
2020-12-08 22:29:26 +0100

    fix database conection for heroku

--- src/config.js
+++ src/config.js
@@ -1,3 +1,13 @@
+/* 
+HEROKU confiuration
+
+CLEARDB_DATABASE_URL: 
+    Host: eu-cdbr-west-03.cleardb.net
+    User: b051a52217a284
+    Root: 520fbacf
+    DB:   heroku_6781bd3fe4580a9
+*/
+
 module.exports = {
     appInfo: {
         name: "Favorite Links",
@@ -9,11 +19,11 @@ module.exports = {
         port: process.env.PORT || 4000,
     },
     database: {
-        host: "localhost",
+        host: "eu-cdbr-west-03.cleardb.net",
         port: "3306",
-        user: "root",
-        password: "Admin_251",
-        database: "database_links",
+        user: "b051a52217a284",
+        password: "520fbacf",
+        database: "heroku_6781bd3fe4580a9",
     },
     auth: {
         secret_text: "EsteEsElTextoParaCodificarClaves",
